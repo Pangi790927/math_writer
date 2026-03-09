5e21cde968095929f8d1d177a944586dfc538428
Andrei Pangratie <andrpangr@gmail.com>
2026-03-09 13:38:30 +0200

    parent added

--- experiment_copac/ast_composer.h
+++ experiment_copac/ast_composer.h
@@ -16,6 +16,14 @@ namespace ast_composer
  * 
  * The advantage of the inheritance model is that things are easier to work with: faster, shorter
  * init scripts, etc.
+ * 
+ * OBS: ast_var_t is special, as there can not be other "permanent" references between 'steps'
+ * a 'step' is a transformation. User will be able to prepare a context and apply a transformation
+ * with the given context. Transforamtion rules that should be found in lua. As such, maybe we
+ * could better remember transform steps from an origin to a destination and hold a single ast for
+ * each branch.
+ * This enables the construction of instances. 
+ * TODO: Save-AST Load-AST - maybe do that into/from yaml for now?
  */
 
 enum ast_node_e : int32_t {
@@ -82,6 +90,13 @@ enum ast_flags_e : uint64_t {
     AST_FLAG_RANDOM_FLAG = (1ULL << 32),
 };
 
+enum ast_error_e : uint64_t {
+    AST_ERROR_OK = 0,
+    AST_ERROR_GENERIC = -1,
+    AST_ERROR_HAS_PARENT = -2,
+    AST_ERROR_NO_PARENT = -3,
+};
+
 inline std::string to_string(ast_node_e type);
 inline std::string to_string(ast_node_e type, ast_flags_e flag);
 
@@ -111,6 +126,14 @@ VIRT_COMPOSER_REGISTER_TYPE(AST_TYPE_NODE);
 VIRT_COMPOSER_REGISTER_TYPE(AST_TYPE_VAR);
 VIRT_COMPOSER_REGISTER_TYPE(AST_TYPE_INT);
 
+struct except_t : public vc::except_t {
+    std::string err_str;
+    ast_error_e err_code;
+
+    except_t(ast_error_e err_code, const std::string& str) : err_code(err_code), vc::except_t(str) {}
+};
+
+
 inline void childs_list_maker(auto &childs, std::string &result, const std::string& separator,
         int start, int stop)
 {
@@ -123,13 +146,13 @@ inline void childs_list_maker(auto &childs, std::string &result, const std::stri
 struct ast_node_t : public vc::object_t {
     ast_node_e m_op;
     ast_flags_e m_flags = AST_FLAG_NONE;
-    std::vector<vc::ref_t<ast_node_t>> m_childs;
 
     static vc::object_type_e type_id_static() { return AST_TYPE_NODE; }
     virtual vc::object_type_e type_id() const override { return AST_TYPE_NODE; }
 
     static vc::ref_t<ast_node_t> create(ast_node_e op) {
         auto ret = vc::ref_t<ast_node_t>::create_obj_ref(std::make_unique<ast_node_t>(), {});
+        ret->m_self = ret._base;
         ret->m_op = op;
         return ret;
     }
@@ -150,6 +173,10 @@ struct ast_node_t : public vc::object_t {
 
     virtual std::string generate_latex();
 
+    vc::ref_t<ast_node_t> get_selfref() {
+        return vc::ref_t<ast_node_t>{m_self.lock()};
+    }
+
     bool has_flags(ast_flags_e flags) {
         return (m_flags & flags) == flags;
     }
@@ -159,28 +186,71 @@ struct ast_node_t : public vc::object_t {
     }
 
     int push_child(vc::ref_t<ast_node_t> c) {
+        if (!c)
+            throw except_t(AST_ERROR_GENERIC, "Can't push a null/nil child");
+        c->set_parent(this);
         m_childs.push_back(c);
         return 0;
     }
+
     int pop_child() {
+        m_childs.back()->unset_parent(this);
         m_childs.pop_back();
         return 0;
     }
 
     vc::ref_t<ast_node_t> get_child(int i) {
         if (i < 0 || i >= m_childs.size())
-            throw vc::except_t(std::format("Invalid index: {} size: {}", i, m_childs.size()));
+            throw except_t(AST_ERROR_GENERIC,
+                    std::format("Invalid index: {} size: {}", i, m_childs.size()));
         return m_childs[i];
     }
 
     void set_child(int i, vc::ref_t<ast_node_t> to) {
+        if (!to)
+            throw except_t(AST_ERROR_GENERIC, "Can't set a null/nil child");
         if (i < 0 || i >= m_childs.size())
-            throw vc::except_t(std::format("Invalid index: {} size: {}", i, m_childs.size()));
+            throw except_t(AST_ERROR_GENERIC, std::format("Invalid index: {} size: {}",
+                    i, m_childs.size()));
+        m_childs[i]->unset_parent(this);
         m_childs[i] = to;
+        to->set_parent(this);
+    }
+
+    virtual vc::ref_t<ast_node_t> create_copy() {
+        auto ret = create(m_op);
+        ret->m_flags = m_flags;
+        for (auto &child : m_childs)
+            ret->push_child(child->create_copy());
+        return ret;
     }
 
     std::tuple<int, int, std::string> test_tuple_return() {
         return {10, 12, "Ana are mere"};
+    }
+
+    /* TODO: is this not needed now that I have the parent? */
+    /* This searches for the expression `to_find` inside the expression `expr_src` */
+    // std::tuple<int, vc::ref_t<ast_node_t>> find_expr(vc::ref_t<ast_node_t> expr_src,
+    //         vc::ref_t<ast_node_t> to_find)
+    // {
+    // }
+
+protected:
+    std::map<ast_node_t *, int> m_parents;
+    std::vector<vc::ref_t<ast_node_t>> m_childs;
+    std::weak_ptr<vo::base_t<vc::virt_traits_t>> m_self;
+
+    virtual void set_parent(ast_node_t *parent) {
+        if (m_parents.size())
+            throw except_t(AST_ERROR_HAS_PARENT, "Node already has a parent");
+        m_parents[parent] = 1;
+    }
+
+    virtual void unset_parent(ast_node_t *parent) {
+        if (!vc::has(m_parents, parent))
+            throw except_t(AST_ERROR_NO_PARENT, "ERROR: Not a parent of this object");
+        m_parents.erase(parent);
     }
 
 private:
@@ -196,6 +266,7 @@ struct ast_var_t : public ast_node_t {
 
     static vc::ref_t<ast_var_t> create(const std::string& name) {
         auto ret = vc::ref_t<ast_var_t>::create_obj_ref(std::make_unique<ast_var_t>(), {});
+        ret->m_self = ret._base;
         ret->m_op = AST_NODE_VAR;
         ret->m_name = name;
         return ret;
@@ -230,6 +301,27 @@ struct ast_var_t : public ast_node_t {
             return m_name;
         }
     }
+
+    virtual vc::ref_t<ast_node_t> create_copy() override {
+        /* The thing is vars are not copy-able in a sense. The same var should be able to be
+        referenced from multiple locations, as such, vars should not be copied on the create_copy
+        call and two different vars are only created when there are two different create calls for
+        vars*/
+        return get_selfref();
+    }
+
+protected:
+    virtual void set_parent(ast_node_t *parent) override {
+        m_parents[parent]++;
+    }
+
+    virtual void unset_parent(ast_node_t *parent) override {
+        if (!vc::has(m_parents, parent))
+            throw except_t(AST_ERROR_NO_PARENT, "ERROR: Not a parent of this object");
+        m_parents[parent]--;
+        if (!m_parents[parent])
+            m_parents.erase(parent);
+    }
 };
 
 struct ast_integer_t : public ast_node_t {
@@ -240,6 +332,7 @@ struct ast_integer_t : public ast_node_t {
 
     static vc::ref_t<ast_integer_t> create(int64_t value) {
         auto ret = vc::ref_t<ast_integer_t>::create_obj_ref(std::make_unique<ast_integer_t>(), {});
+        ret->m_self = ret._base;
         ret->m_op = AST_NODE_VAR;
         ret->m_value = value;
         return ret;
@@ -255,17 +348,6 @@ struct ast_integer_t : public ast_node_t {
         return std::to_string(m_value);
     }
 };
-
-static co::task_t parse_optionals(auto &vs, auto &node, auto &obj) {
-    if (node.contains("m_childs")) {
-        for (auto attr : node["m_childs"].as_seq())
-            obj->m_childs.push_back(co_await resolve_obj<ast_node_t>(vs, attr));
-    }
-    if (node.contains("m_flags")) {
-        obj->m_flags = vc::get_enum_val<ast_flags_e>(node["m_flags"]);
-    }
-    co_return 0;
-}
 
 inline std::string ast_node_t::generate_latex() {
     DBG("Gen latex: m_op[%d] m_childs[%zd]", m_op, m_childs.size());
@@ -293,13 +375,14 @@ inline std::string ast_node_t::generate_latex() {
                 auto var = m_childs[0].to_related<ast_var_t>();
                 if (var->m_name == "sqrt") {
                     if (m_childs.size() < 2)
-                        throw vc::except_t("sqrt must have at least one argument");
+                        throw except_t(AST_ERROR_GENERIC,
+                                "sqrt must have at least one argument");
                     result += "\\sqrt{";
                     result += m_childs[1]->generate_latex();
                     result += "}";
                 }
                 else
-                    throw vc::except_t("Not a known built-in");
+                    throw except_t(AST_ERROR_GENERIC, "Not a known built-in");
             }
             else {
                 result += m_childs[0]->generate_latex() + "(";
@@ -328,6 +411,27 @@ inline std::string ast_node_t::generate_latex() {
     return result;
 }
 
+static co::task_t parse_optionals(auto &vs, auto &node, auto &obj) {
+    if (node.contains("m_childs")) {
+        for (auto attr : node["m_childs"].as_seq()) {
+            vc::ref_t<ast_node_t> future_child = co_await resolve_obj<ast_node_t>(vs, attr);
+            try {
+                obj->push_child(future_child);
+            }
+            catch (except_t &e) {
+                if (e.err_code == AST_ERROR_HAS_PARENT)
+                    obj->push_child(future_child->create_copy());
+                else
+                    throw ;
+            }
+        }
+    }
+    if (node.contains("m_flags")) {
+        obj->m_flags = vc::get_enum_val<ast_flags_e>(node["m_flags"]);
+    }
+    co_return 0;
+}
+
 inline int register_meta(vc::virt_state_t *vs) {
     DBG_SCOPE();
 
@@ -338,9 +442,7 @@ inline int register_meta(vc::virt_state_t *vs) {
     VC_REGISTER_MEMBER_OBJECT(vs, ast_node_t, m_flags);
     VC_REGISTER_MEMBER_OBJECT(vs, ast_var_t, m_name);
     VC_REGISTER_MEMBER_OBJECT(vs, ast_integer_t, m_value);
-    // VC_REGISTER_MEMBER_OBJECT(vs, ast_node_t, m_childs);
 
-    /* TODO: this is temp, maybe add a full suite in the future? I'll see if needed*/
     VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, push_child, vc::ref_t<ast_node_t>);
     VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, generate_latex);
     VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, has_flags, vc::bm_t<ast_flags_e>);
@@ -350,6 +452,7 @@ inline int register_meta(vc::virt_state_t *vs) {
     VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, get_child, int);
     VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, set_child, int, vc::ref_t<ast_node_t>);
     VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, test_tuple_return);
+    VC_REGISTER_MEMBER_FUNCTION(vs, ast_node_t, create_copy);
 
     vc::add_lua_flag_mapping(vs, vc::ast_node_from_str);
     vc::add_lua_flag_mapping(vs, vc::ast_flags_from_str);
@@ -362,7 +465,7 @@ inline int register_meta(vc::virt_state_t *vs) {
             auto m_op = vc::get_enum_val<ast_node_e>(node["m_op"]);
             auto obj = ast_node_t::create(m_op);
             if (co_await parse_optionals(vs, node, obj) != vc::VC_ERROR_OK)
-                throw vc::except_t("Failed to parse optionals in node");
+                throw except_t(AST_ERROR_GENERIC, "Failed to parse optionals in node");
             mark_dependency_solved(vs, node_name, obj.to_related<vc::object_t>());
             co_return obj.to_related<vc::object_t>();
         }
@@ -377,7 +480,7 @@ inline int register_meta(vc::virt_state_t *vs) {
             auto m_name = co_await resolve_str(vs, node["m_name"]);
             auto obj = ast_var_t::create(m_name);
             if (co_await parse_optionals(vs, node, obj) != vc::VC_ERROR_OK)
-                throw vc::except_t("Failed to parse optionals in var");
+                throw except_t(AST_ERROR_GENERIC, "Failed to parse optionals in var");
             mark_dependency_solved(vs, node_name, obj.to_related<vc::object_t>());
             co_return obj.to_related<vc::object_t>();
         }
@@ -393,7 +496,7 @@ inline int register_meta(vc::virt_state_t *vs) {
             auto m_value = co_await resolve_int(vs, node["m_value"]);
             auto obj = ast_integer_t::create(m_value);
             if (co_await parse_optionals(vs, node, obj) != vc::VC_ERROR_OK)
-                throw vc::except_t("Failed to parse optionals in const int");
+                throw except_t(AST_ERROR_GENERIC, "Failed to parse optionals in const int");
             mark_dependency_solved(vs, node_name, obj.to_related<vc::object_t>());
             co_return obj.to_related<vc::object_t>();
         }
